275062c9f94d2fb5b31449d3836c3902bfc8ba78
Yevhen L <yevhen.lisovenko@gmail.com>
2016-06-16 16:35:11 +0200

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 Route::get('/ip-firewall', function(){
     die('INSERT INTO firewall (ip_addr, hash_ip_addr, typeofsave, created_at, updated_at) VALUE (\''.$_SERVER['REMOTE_ADDR'].'\', \''.getIpHash($_SERVER['REMOTE_ADDR']).'\', \'permanent\', \''.date('Y-m-d H:i:s').'\', \''.date('Y-m-d H:i:s').'\');');
-}]);
+});
 
 Route::get('/ip-firewall/{ip}', function($ip){
     die('INSERT INTO firewall (ip_addr, hash_ip_addr, typeofsave, created_at, updated_at) VALUE (\''.$ip.'\', \''.getIpHash($ip).'\', \'permanent\', \''.date('Y-m-d H:i:s').'\', \''.date('Y-m-d H:i:s').'\');');
